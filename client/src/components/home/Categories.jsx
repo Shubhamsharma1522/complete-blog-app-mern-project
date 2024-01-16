36e0d3ca2fa094e17c7cb4c1c0ed68bda4bb3e19
Shubhamsharma1522 <shubhamsharma.in1522@gmail.com>
2024-01-17 02:25:37 +0530

    stylling

--- client/src/components/home/Categories.jsx
+++ client/src/components/home/Categories.jsx
@@ -13,12 +13,14 @@ import { categories } from "../../constants/data";
 
 const StyledTable = styled(Table)`
   // border: 2px solid rgba(224, 224, 224, 1);
-  border: 2px solid white;
-  background: white;
+  margin-top: 20px;
+  border: 2px solid black;
+  background: #f8f8f8;
 `;
 
 const StyledButton = styled(Button)`
-  margin: 10px;
+  margin: 0px 20px 4px 5px;
+  padding: 10px 20px 10px 20px;
   color: black;
   border: 1px solid black;
   border-radius: 10px;
@@ -31,12 +33,16 @@ const StyledLink = styled(Link)`
   color: #000;
 `;
 const StyledLinkCategory = styled(Link)`
-  text-decoration: none;
-  color: #000;
-  padding: 8px;
-  margin-top: 7px;
+  // text-decoration: none;
+  // color: #000;
+  padding: 10px 20px 10px 20px;
+  // margin: 12px;
+  margin-top: 10px;
   margin-left: 10px;
   display: flex;
+  width: 140px;
+  border: 1px solid black;
+  border-radius: 10px;
 `;
 
 const Categories = () => {
@@ -53,13 +59,13 @@ const Categories = () => {
         >
           Create Blog
         </StyledButton>
+        <StyledLinkCategory className="hover:bg-red-700" to="/">
+          All Categories
+        </StyledLinkCategory>
       </StyledLink>
 
       <StyledTable>
         <TableBody>
-          <StyledLinkCategory className="hover:text-red-700" to="/">
-            All Categories
-          </StyledLinkCategory>
           {categories.map((category) => (
             <TableCell key={category.id}>
               <TableRow>
